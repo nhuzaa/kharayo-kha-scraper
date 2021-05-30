12fae6b48dfca0f0de394b2b859c567021aa4c79
nhuzaa <nhuzaa@gmail.com>
2021-05-30 20:05:15 +0545

    shell script

--- jobs.sh
+++ jobs.sh
@@ -2,7 +2,6 @@
 
 cd /app/scraper
 uvicorn wakeup:app --host 0.0.0.0 --port 8000 &&
-while true; do
-  scrapy crawl onlinekhabar && scrapy crawl ekantipur
-  sleep 1200 
-done
+
+ scrapy list|xargs -n 1 scrapy crawl
+
